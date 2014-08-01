7994dfaab9441d525fdcd22a6faa36448b2c094b
MATSUKI.Hidenori <MATSUKI.Hidenori@gmail.com>
2014-08-01 20:07:12 +0900

    Update benchmark script for shorten the run time.

--- 02_benchmark_fio/runner.sh
+++ 02_benchmark_fio/runner.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 declare pid=$$
+declare dstatpid=-1
 declare executedate=$(date +%Y%m%d_%H%M%S)
 declare workdir="${PWD}/executed.$(hostname).${executedate}.${pid}"
 declare device=hioa
 declare -a partitions=('hioa1' 'hioa2' 'hioa3' 'hioa4' 'hioa5')
-declare -a filesystems=('ext4' 'xfs' 'btrfs' 'jfs' 'ext4' 'xfs' 'btrfs' 'jfs' 'ext4' 'xfs' 'btrfs' 'jfs' 'ext4' 'xfs' 'btrfs' 'jfs' 'ext4' 'xfs' 'btrfs' 'jfs')
+declare -a filesystems=('xfs' 'btrfs' 'jfs' 'ext4' 'xfs' 'btrfs' 'jfs' 'ext4' 'xfs' 'btrfs' 'jfs' 'ext4' 'xfs' 'btrfs' 'jfs' 'ext4' 'xfs' 'btrfs' 'jfs' 'ext4')
 
 mkdir -p "${workdir}"
-trap 'echo "Error occurred!" >> "${workdir}/error.txt"; for e in ${partitions[@]}; do umount -f /mnt/${partition} >> ${workdir}/error.txt; done' EXIT
+trap 'echo "Error occurred!" >> "${workdir}/error.txt"; for e in ${partitions[@]}; do umount -f /mnt/${partition} >> ${workdir}/error.txt; done; kill ${dstatpid}' EXIT
 
 echo -n 'Create partitions... '
 gdisk /dev/${device} >/dev/null <<EOF
@@ -71,18 +72,22 @@ for ((i=0; i<${#filesystems[@]}; i++)) {
   mount | grep "/mnt/${partition}" | tee "${workdir}/mount.${turnname}.txt"
   df /mnt/${partition} | tee "${workdir}/df.${turnname}.txt"
   echo -n 'Running benchmark...'
-  for ioengine in sync libaio mmap
+  for ioengine in libaio mmap sync
   do
     for readwrite in read write randread randwrite
     do
-      for blocksize in 1k 2k 4k 8k 16k
+      for blocksize in 2k 4k 8k
       do
-        for ((numjobs=1; numjobs<=64;)) {
-          for ((iodepth=1; iodepth<=64;)) {
+        for ((numjobs=1; numjobs<=32;)) {
+          for ((iodepth=1; iodepth<=32;)) {
             jobname="job.${turnname}.${ioengine}.${readwrite}.bs${blocksize}.j${numjobs}.depth${iodepth}"
-            dstat --cpu --disk --page --int --load --mem --proc --io --swap --time --sys --aio --fs --ipc --lock --raw --socket --unix --vm --output "${workdir}/dstat.${jobname}.csv" > /dev/null &
+            dstat --epoch --time --cpu --disk --page --int --load --mem --proc --io --swap --sys --aio --fs --ipc --lock --raw --socket --unix --vm --output "${workdir}/dstat.${jobname}.csv" > /dev/null &
             dstatpid=$!
-            fio -filename=/mnt/${partition}/fio.bin -ioengine=${ioengine} -direct=1 -invalidate=1 -readwrite=${readwrite} -blocksize=${blocksize} -size=1G -numjobs=${numjobs} -iodepth=${iodepth} -group_reporting -name=${jobname} | tee "${workdir}/fio.${jobname}.txt"
+            sleep 15
+            date +"%d-%m %X %s" | tee -a "${workdir}/fio.${jobname}.txt"
+            time fio --runtime=240 -filename=/mnt/${partition}/fio.bin -ioengine=${ioengine} -direct=1 -invalidate=1 -readwrite=${readwrite} -blocksize=${blocksize} -size=1G -numjobs=${numjobs} -iodepth=${iodepth} -group_reporting -name=${jobname} | tee -a "${workdir}/fio.${jobname}.txt"
+            date +"%d-%m %X %s" | tee -a "${workdir}/fio.${jobname}.txt"
+            sleep 15
             kill ${dstatpid}
             iodepth=$((iodepth * 2))
           }
